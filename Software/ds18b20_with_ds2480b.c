5b95527a1f4fa6360c04381a9b42384997b2bf85
auomoto <auomoto@gmail.com>
2016-02-08 12:20:05 -0800

    Fixed negative temperatures again

--- Software/ds18b20_with_ds2480b.c
+++ Software/ds18b20_with_ds2480b.c
@@ -1,3 +1,21 @@
+/*
+	This program reads temperatures from a string of DS18B20 digital sensors
+	through a DS2480B 1-Wire to serial interface chip. The serial interface
+	chip handles the 1-Wire network timing so non-real-time computers can
+	use the temperature sensors. The DS2480B also allows pulse-shape control
+	that can improve long-distance performance on a 1-Wire network.
+
+	The DS2480B data sheet and the Maxim Application Note 192 have information
+	on how to use this device.
+
+	This program is known to compile with gcc but should work with any POSIX.
+	The development environment is Cygwin on Windows 10 or Debian on a Raspberry PI.
+	
+	$ cc ds18b20_with_ds2480b.c
+	$ ./a.exe /dev/ttyS2
+
+*/
+	
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
@@ -5,10 +23,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+// Change these if you'd like
 #define DEFAULTSERIAL	"/dev/ttyS2"
-#define	msleep(t)	usleep(1000*t)		// Milliseconds sleep
+#define MAXDEVICES		25				// Array size, # of 1-Wire devices
 
-// DS2480B
+// DS2480B serial to 1-Wire driver
 #define COMMANDMODE		0xE3
 #define DATAMODE		0xE1
 #define RESETCOMMAND	0xC5			// DS2480B flex speed reset
@@ -18,17 +37,19 @@
 
 // 1-Wire
 #define MATCHROM		0x55
-#define MAXDEVICES		25
 #define READROM			0x33
 #define	SEARCHROM		0xF0
 #define SKIPROM			0xCC
 
-// DS18B20
+// DS18B20 temperature sensor
 #define CONVERT			0x44
 #define READSCRATCHPAD	0xBE
 
+// Helpers
+#define	msleep(t)	usleep(1000*t)		// Milliseconds sleep
+
 // Globals
-uint8_t currentMode;
+uint8_t currentMode;					// Command or Data mode flag for the DS2480B
 
 // Function Prototypes
 int ds18b20_convert(int);
@@ -57,28 +78,41 @@ int serialInit(char*);
 void setDirectionBit(int, uint8_t*);
 uint8_t twoIntoOne(uint8_t*);
 
+/*------------------------------------------------------------------------------
+	This program starts by scanning the 1-Wire bus for all devices and saving
+	their ROM addresses. Then for every DS18B20 device (those with a first
+	address byte of 0x28) the temperature is read out and printed on stdout
+	along with the POSIX time and 1-Wire ROM address.
+
+	For G-CLEF, we probably want to hold the ROM addresses in a database along
+	with the location of each sensor.
+------------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
 
 	uint8_t romAddresses[MAXDEVICES][8];
-	int i, j, fd, nDevices, currentTime;
+	int i, fd, nDevices, currentTime;
 	float Celsius, Fahrenheit;
 
-	fd = initialize(argc, argv);			// Open the DS2480B serial port
-	nDevices = scanBus(fd, romAddresses);
-	for (;;) {
-		ds2480b_skipROM(fd);				// Address all sensors
-		currentTime = (int) time(NULL);		// POSIX time (no leap seconds)
-		ds18b20_convert(fd);				// All sensors A/D conversion to temperature
+	fd = initialize(argc, argv);				// Open the DS2480B serial port
+	if (fd < 0) {
+		fprintf(stderr, "DS2480B detect failed.\n");
+		return(0);
+	}
+
+	nDevices = scanBus(fd, romAddresses);		// Get all 1-Wire addresses
+
+	for (;;) {									// Loop forever, reading and printing temperatures
+		ds2480b_skipROM(fd);					// Address all sensors
+		currentTime = (int) time(NULL);			// POSIX time (no leap seconds)
+		ds18b20_convert(fd);					// Ask all sensors to save temperature
 		for (i = 0; i < nDevices; i++) {
 			if (romAddresses[i][0] == 0x28) {	// 0x28 is the DS18B20 device family
 				ds2480b_matchROM(fd, romAddresses[i]);
 				Celsius = ds18b20_readTemperature(fd);
 				Fahrenheit = 32.0 + 9.0 * Celsius / 5.0;
-				printf("%d %6.2f %5.1f [ ", currentTime, Celsius, Fahrenheit);
-				for (j = 0; j < 8; j++) {
-					printf("%02X ", romAddresses[i][j]);
-				}
+				printf("%d %9.5f %5.1f [ ", currentTime, Celsius, Fahrenheit);
+				prROM(romAddresses[i]);
 				printf("]\n");
 				fflush(stdout);
 			}
@@ -92,8 +126,11 @@ int main(int argc, char *argv[])
 	int initialize(argc, argv)
 
 	Open the serial port connected to the DS2480B. DEFAULTSERIAL port is used
-	unless a port is named on the command line (anything with a leading '/' will
-	be interpreted as a serial port).
+	unless a port is named on the command line. Anything with a leading '/' will
+	be interpreted as a serial port.
+	
+	After the serial port is opened, do a DS2480B DETECT command. This sets up
+	the DS2480B by setting the pulse shaping parameters and baud rate.
 ------------------------------------------------------------------------------*/
 int initialize(int argc, char *argv[])
 {
@@ -109,9 +146,7 @@ int initialize(int argc, char *argv[])
 	}
 	fd = serialInit(portName);	// Open the serial port
 	if (ds2480b_detect(fd) == -1) {
-		fprintf(stderr, "initialize: first ds2480b_detect failed\n");
-		msleep(100);
-		ds2480b_detect(fd);
+		return(-1);
 	}
 	return(fd);
 
@@ -120,8 +155,13 @@ int initialize(int argc, char *argv[])
 /*------------------------------------------------------------------------------
 	int scanBus(fd, romAddresses)
 
-	Scans the 1-Wire bus and puts the addresses into romAddress. Returns the
-	number of devices found on the bus.
+	Scans the 1-Wire bus putting the addresses into romAddress and returning the
+	number of devices found.
+
+	We use the 64-byte directions[] array to store the idBit path to take and to
+	receive discrepancy flags for the searchROM function. The actual 16-byte data
+	arrays sent to and received from the DS2480B are packed and unpacked from the
+	directions array at time of use.
 ------------------------------------------------------------------------------*/
 int scanBus(int fd, uint8_t romAddresses[][8])
 {
@@ -147,6 +187,7 @@ int scanBus(int fd, uint8_t romAddresses[][8])
 		}
 	}
 	return(nDevices);
+
 }
 
 /*------------------------------------------------------------------------------
@@ -163,16 +204,16 @@ int serialInit(char *portName)
 
 	fd = open(portName, O_RDWR | O_NOCTTY | O_NDELAY);
 	if (fd < 0) {
-		fprintf(stderr, "initialize: can't open serial port %s\n", portName);	
+		fprintf(stderr, "serialInit: can't open serial port %s\n", portName);	
 		exit(1);
 	}
-	flags = fcntl(fd, F_GETFL);			// retrieve access mode and status flags
-	flags |= O_NONBLOCK;				// set non-blocking
-	fcntl(fd, F_SETFL, flags);			// save new flags
-	tcgetattr(fd, &tty_attrib);			// retrieve current attributes
-	cfsetspeed(&tty_attrib, B9600);		// set baud rate
-	cfmakeraw(&tty_attrib);				// set raw mode
-	tcflush(fd, TCIOFLUSH);				// flush read and write data
+	flags = fcntl(fd, F_GETFL);				// retrieve access mode and status flags
+	flags |= O_NONBLOCK;					// set non-blocking
+	fcntl(fd, F_SETFL, flags);				// save new flags
+	tcgetattr(fd, &tty_attrib);				// retrieve current attributes
+	cfsetspeed(&tty_attrib, B9600);			// set baud rate
+	cfmakeraw(&tty_attrib);					// set raw mode
+	tcflush(fd, TCIOFLUSH);					// flush read and write data
 	tcsetattr(fd, TCSANOW, &tty_attrib);	// save new attributes
 	return(fd);
 
@@ -181,7 +222,7 @@ int serialInit(char *portName)
 /*------------------------------------------------------------------------------
 	void serialBreak(fd)
 
-	Send a break to the serial port connected to fd.
+	Send a break to the serial port on fd.
 ------------------------------------------------------------------------------*/
 void serialBreak(int fd)
 {
@@ -197,14 +238,14 @@ void serialBreak(int fd)
 /*------------------------------------------------------------------------------
 	int ds18b20_convert(fd)
 
-	Send the convert command (0x44) to the DS18B20 sensor and read
-	back the reply (command echo) from the DS2480B bridge. Returns -1
-	on error. This includes the 750ms conversion time wait.
+	Send the convert command (0x44) to the DS18B20 sensor and read back the
+	reply (a command echo) from the DS2480B bridge. Returns -1 on error. This
+	includes the 750ms conversion time wait.
 ------------------------------------------------------------------------------*/
 int ds18b20_convert(int fd)
 {
 
-	uint8_t tbuf[3];
+	uint8_t tbuf[1];
 	int n;
 	
 	ds2480b_mode(fd, DATAMODE);
@@ -231,14 +272,13 @@ int ds18b20_convert(int fd)
 /*------------------------------------------------------------------------------
 	float ds18b20_readTemperature(fd)
 
-	Returns the floating point value. Do a CONVERT first.
+	Returns the floating point temperature value in C. Do a CONVERT first.
 ------------------------------------------------------------------------------*/
 float ds18b20_readTemperature(int fd)
 {
 
 	uint8_t i, n, tbuf[9];
-	int C;
-	float sign;
+	int16_t C;
 
 	ds2480b_mode(fd, DATAMODE);
 	tbuf[0] = READSCRATCHPAD;
@@ -256,22 +296,16 @@ float ds18b20_readTemperature(int fd)
 		fprintf(stderr, "ds2480b_readTemperature: error receiving scratchpad data\n");
 		return(-1);
 	}
-	C = tbuf[2];
+	C = (uint16_t) tbuf[2];
 	C = C << 8;
-	C += tbuf[1];
-	// C = 0xFE6F;		// Test value -25.00
-	if (C & 0x8000) {
-		sign = -1.0;
-		C ^= 0xFFFF;
-	} else {
-		sign = 1.0;
-	}
-	return(sign * 0.0625 * (float) C);
+	C |= (uint16_t) tbuf[1];
+//C = 0xFE6F;		// Test value for -25.0625
+//C = 0x0191;		// Test value for +25.0625
+//C = 0xFFF8;		// Test value for -0.5
+	return(0.0625 * (float) C);
 
 }
 /* END DS18B20 ===============================================================*/
-
-
 
 /* BEGIN DS2480B =============================================================*/
 /*------------------------------------------------------------------------------
@@ -287,18 +321,18 @@ float ds18b20_readTemperature(int fd)
 int ds2480b_detect(fd)
 {
 
-	uint8_t i, n, tbuf[6], response[] = {0x16, 0x44, 0x5A, 0x00, 0x93};
+	uint8_t i, n, tbuf[5], response[] = {0x16, 0x44, 0x5A, 0x00, 0x93};
 
 	serialBreak(fd);			// Break resets the DS2480B
 	tbuf[0] = TIMINGBYTE;
 	ds2480b_send(fd, tbuf, 1);	// Send the timing byte
 
-							// Pulse shaping & detect: See p4 & p5 of AN192
-	tbuf[0] = 0x17;			// Pull-down slew rate (PDSRC) 1.37V/us
-	tbuf[1] = 0x45;			// Write-low time 10 us (W1LT or W1LD)
-	tbuf[2] = 0x5B;			// DSO/WORT 8 us
-	tbuf[3] = 0x0F;			// Read baud rate register command request
-	tbuf[4] = 0x91;			// 1-Wire bit
+								// Pulse shaping & detect: See p4 & p5 of AN192
+	tbuf[0] = 0x17;				// Pull-down slew rate (PDSRC) 1.37V/us
+	tbuf[1] = 0x45;				// Write-low time 10 us (W1LT or W1LD)
+	tbuf[2] = 0x5B;				// DSO/WORT 8 us
+	tbuf[3] = 0x0F;				// Read baud rate register command request
+	tbuf[4] = 0x91;				// 1-Wire bit
 
 	// Send the commands
 	n = ds2480b_send(fd, tbuf, 5);
@@ -332,12 +366,13 @@ int ds2480b_detect(fd)
 	}
 	currentMode = COMMANDMODE;
 	return(0);
+
 }
 
 /*------------------------------------------------------------------------------
 	int ds2480b_matchROM(fd, addr)
 
-	Send the match ROM command
+	Send the matchROM command. Communicate with the selected device only.
 ------------------------------------------------------------------------------*/
 int ds2480b_matchROM(int fd, uint8_t *addr)
 {
@@ -383,6 +418,7 @@ int ds2480b_mode(int fd, uint8_t mode)
 	if (currentMode == mode) {
 		return(0);
 	}
+
 	tbuf[0] = mode;
 	n = ds2480b_send(fd, tbuf, 1);
 	if (n == -1) {
@@ -405,17 +441,17 @@ int ds2480b_mode(int fd, uint8_t mode)
 		vvv	- DS2480 version
 			- 010 means DS2480
 			- 011 means DS2480B
-		rr	- 00 shorted
+		rr	- 00 shorted 1-Wire bus
 			- 01 presence detected (OK)
-			- 10 alarm detected
-			- 11 no presence detected
+			- 10 alarm detected on a device
+			- 11 no presence pulses detected
 
 	A good response is 0xCD
 ------------------------------------------------------------------------------*/
 int ds2480b_reset(int fd)
 {
 
-	uint8_t tbuf[2];
+	uint8_t tbuf[1];
 	int n;
 
 	if (currentMode == DATAMODE) {
@@ -453,7 +489,8 @@ int ds2480b_reset(int fd)
 /*------------------------------------------------------------------------------
 	int ds2480b_readROM(fd, addr)
 
-	Read the ROM address of a single 1-Wire device on the bus and print it.
+	If there is only one 1-Wire device on the bus, read its address and
+	return it.
 ------------------------------------------------------------------------------*/
 int ds2480b_readROM(int fd, uint8_t *addr)
 {
@@ -480,10 +517,6 @@ int ds2480b_readROM(int fd, uint8_t *addr)
 	for (i = 0; i < 8; i++) {
 		addr[i] = tbuf[i+1];
 	}
-	for (i = 0; i < 8; i++) {
-		printf("%02X ", addr[i]);
-	}
-	printf("\n");
 	return(0);
 
 }
@@ -492,7 +525,10 @@ int ds2480b_readROM(int fd, uint8_t *addr)
 	int ds2480b_recv(fd, tbuf, nbytes)
 
 	Receive a stream of bytes from the serial port. Returns the number of bytes
-	or -1 if there's an error.
+	or -1 if there's an error. There's an msleep() at the top of this routine
+	to give commands sent immediately before time to finish. Not all commands
+	take the same amount of time but 50ms seems to be long enough to cover the
+	DS18B20 situations.
 ------------------------------------------------------------------------------*/
 int ds2480b_recv(int fd, uint8_t *tbuf, int nbytes)
 {
@@ -514,7 +550,17 @@ int ds2480b_recv(int fd, uint8_t *tbuf, int nbytes)
 /*------------------------------------------------------------------------------
 	int ds2480b_send(fd, data, nbytes)
 
-	Send a stream of bytes out the serial port. Returns -1 on error.
+	Send a stream of bytes out the serial port. Returns -1 on error. The longest
+	byte stream we've needed for the DS18B20 is 16 bytes, needed to send and
+	receive the searchROM data. Because sending a 0xE3 byte (COMMANDMODE) as
+	data requires sending it twice in succession tbuf is sized 32 bytes for
+	the worst-case (likely impossible) situation.
+	
+	There's a 50ms sleep after sending to give devices a chance to react. This
+	is much longer than commonly needed but pretty much guarantees we won't ever
+	try to read back data too soon. Combined with the 50ms wait at the top of
+	a read data sequence, we have a minimum 0.1 seconds between the start of a
+	send data and the end of a receive data sequence.
 ------------------------------------------------------------------------------*/
 int ds2480b_send(int fd, uint8_t *data, int nbytes)
 {
@@ -546,7 +592,8 @@ int ds2480b_send(int fd, uint8_t *data, int nbytes)
 /*------------------------------------------------------------------------------
 	int ds2480b_skipROM(fd)
 
-	Sends the skip ROM command. Returns -1 on error.
+	Sends the skip ROM command, telling every device on the bus to listen.
+	Returns -1 on error.
 ------------------------------------------------------------------------------*/
 int ds2480b_skipROM(int fd)
 {
@@ -582,11 +629,11 @@ int ds2480b_skipROM(int fd)
 /*------------------------------------------------------------------------------
 	ds2480b_searchROM(fd, directions, addr)
 
-	Retrieve the next 1-Wire device address. The directions array controls
-	the path taken when a discrepancy is noted. The directions array is
-	modified within this routine to reflect the response from the searchROM,
-	that is, it's equivalent to the response from the searchROM command but
-	packed in an easier-to-use format.
+	Retrieve the next 1-Wire device address during a searchROM loop. The
+	directions array controls the path taken when a discrepancy is noted.
+	The directions array is modified within this routine to reflect the
+	response from the searchROM, that is, it's equivalent to the response
+	from the searchROM command but packed in an easier-to-use format.
 ------------------------------------------------------------------------------*/
 void ds2480b_searchROM(int fd, uint8_t *directions, uint8_t *addr)
 {
@@ -607,21 +654,21 @@ void ds2480b_searchROM(int fd, uint8_t *directions, uint8_t *addr)
 	ds2480b_send(fd, cmd, 1);		// No reply expected
 	ds2480b_mode(fd, DATAMODE);
 	loadBuf(directions, tbuf);		// Create an output data buffer
-	ds2480b_send(fd, tbuf, 16);		// Send 16 output data bytes
-	ds2480b_recv(fd, tbuf, 16);		// Receive 16 reply data bytes
+	ds2480b_send(fd, tbuf, 16);		// Send 16 byte output data array
+	ds2480b_recv(fd, tbuf, 16);		// Receive 16 byte reply data array
 	parse(directions, tbuf);		// Unpack the reply into the directions array
-	getROM(addr, tbuf);				// Unpack the ROM address found
+	getROM(addr, tbuf);				// Unpack the ROM address
 	ds2480b_mode(fd, COMMANDMODE);
 	cmd[0] = SEARCHACCOFF;			// Turn off the search accelerator
 	ds2480b_send(fd, cmd, 1);		// No reply expected
-	ds2480b_reset(fd);
+	ds2480b_reset(fd);				// Reset the 1-Wire bus
 
 }
 
 /*------------------------------------------------------------------------------
 	getROM(romAddr, tbuf)
 
-	Extracts the ROM address from a response array.
+	Extracts the ROM address from a searchROM response array.
 ------------------------------------------------------------------------------*/
 void getROM(uint8_t *romAddr, uint8_t *tbuf)
 {
@@ -679,13 +726,12 @@ void loadBuf(uint8_t *directions, uint8_t *tbuf)
 	parse(directions, response)
 
 	Takes the response from a searchROM command and creates the directions
-	array. The directions array then contains the 2-bits of information for
-	each idBit.
+	array. The each directions array byte then contains the 2-bits of
+	information for each idBit.
 ------------------------------------------------------------------------------*/
 void parse(uint8_t *directions, uint8_t *response)
 {
 
-	
 	uint8_t tmp;
 	int i, j, idBit;
 
@@ -697,12 +743,13 @@ void parse(uint8_t *directions, uint8_t *response)
 			idBit++;
 		}
 	}
+
 }
 
 /*------------------------------------------------------------------------------
 	prArray(array)
 
-	Prints the 16-byte output data and response array (for debugging).
+	Prints the 16-byte output data and response array.
 ------------------------------------------------------------------------------*/
 void prArray(uint8_t *array)
 {
@@ -720,12 +767,13 @@ void prArray(uint8_t *array)
 			printf("(%03d):%d,%d ", abit, r, d);
 		}
 	}
+
 }
 
 /*------------------------------------------------------------------------------
 		prParsed(array)
 
-		Prints the directions array (for debugging).
+		Prints the directions array.
 ------------------------------------------------------------------------------*/
 void prParsed(uint8_t *array)
 {
@@ -758,7 +806,7 @@ void prROM(uint8_t *addr)
 	for (i = 0; i < 8; i++) {
 		printf("%02X ", addr[i]);
 	}
-	printf("\n");
+
 }
 
 /*------------------------------------------------------------------------------
